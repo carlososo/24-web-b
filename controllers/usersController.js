4c1371def94914356de4a9cbbbb114df47bdf821
Carlos Osorio <carlosoc@live.com>
2023-10-24 22:35:14 -0500

    controller create read

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,23 +1,43 @@
-const {response, request} = require("express")
+const {response, request} = require("express");
+const User = require("../models/usersModel");
 
-const createUser = (req = request, res = response) => {
-  const { body } = req;
-  
-  console.log(body)
-  res.json({
-    msg:"Hola desde el Controller file"
-  })
+const createUser = async(req = request, res = response) => {
+  try{
+    const { body } = req;
+    const user = new User(body)
+    await user.save()
+
+    res.status(201).json({
+      user
+    })
+  }catch(error){
+    res.status(500).json({
+      msg:"Algo Ocurrio al crear un usuario",
+      error
+    })
+  }
 }
-
-const readUser = (req, res) => {
-  res.json({
-    msg: "Leer usuarios desde Controller"
-  })
+const readUser = async(req, res) => {
+  try {
+    const { limit = 10 } = req.query
+    const queryParam = {active:true}
+    const recordLength = await User.countDocuments()
+    const user = await User.find(queryParam).limit(Number(limit));
+    res.json({
+      recordLength,
+      user
+    })
+  } catch (error) {
+    res.status(500).json({
+      msg:"Algo Ocurrio al leer usuarios",
+      error
+    })  
+  }
+  
 }
 
 const updateUser = (req = request, res) => {
   const { params, query } = req
-  console.log(query)
   res.json({
     msg:"Modificar usuarios desde controller"
   })
